75b8e0d9dd29565711b16fc16267e272eacca390
AmirMousavi <amirrmousavi10@gmial.com>
2026-05-26 10:09:25 +0330

    feat(setting):set setting

---
+++ src/config/setting.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str
+    PRICE_EXCEL_PATH: str
+    CHROMA_DB_DIR: str
+    ALIAS_PATH: str
+    EMBEDDING_MODEL: str = "text-embedding-3-small"
+
+    model_config = SettingsConfigDict(env_file=".env",extra="ignore")
+
+settings = Settings()
+
